56f696a01935b6c270ae960aacf30191a83e97b0
Hugh Robert Labusohr The 2nd <hughlabusohr@mac.com>
2016-03-16 12:48:38 -0400

    Finish converting vanilla to jquery

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -15,30 +15,65 @@
 //= require d3
 //= require_tree .
 
-  function disableButton() {
-    var button = document.getElementsByClassName("actions");
-    button.lastElementChild.disabled = 'true';
+function disableButton(button) {
+  // var button = document.getElementsByClassName("actions");
+  // button.lastElementChild.disabled = 'true';
+  // submitButton.form.submit();
+  $(button).prop("disabled", true);
+  $(button).closest("form").submit();
+};
+$(function bindSubmitClick() {
+  $("input[type=submit]").on("click", disableButton);
+};
+$(bindSubmitClick);
 
-    submitButton.form.submit();
-  };
-
-  function hideRow() {
-    var row = document.getElementsByClassName("row");
-    row[row.length - 1].style.visibility = "hidden";
-  };
-
-function showLastRow() {
-  var row = document.getElementsByClassName("row");
-  row[row.length - 1].style.visibility = "block";
+function hideRow() {
+    // var row = document.getElementsByClassName("row");
+    // row[row.length - 1].style.visibility = "hidden";
+    var row = $(event.target).closest(".row")
+    row.css("display", "none")
 };
 
+function showLastRow() {
+  var row = $(event.target).closest(".row")
+  row.css("visibility", "block");
+};
+
+// Delete Last Row
 function deleteLastRow() {
-  var row = document.getElementsByClassName("row");
-  var row = event.target.parentNode.parentNode;
-  row.style.display = "none";
-  var check_box = row.lastElementChild.lastElementChild;
-  checkbox.selected
+  // var row = document.getElementsByClassName("row");
+  // // var row = event.target.parentNode.parentNode;
+  // row.style.display = "none";
+  // var check_box = row.lastElementChild.lastElementChild;
+  // checkbox.selected;
+  var row = $(event.target).closest(".row")
+  row.css("display", "none")
+  var checkbox = $(event.target).siblings().last();
+  checkbox.prop("checked", true);
 }
+
+// Display Grade
+function displayGrade(displayType) {
+  // var associations = document.getElementbyId("associations");
+  var associations = $("#associations");
+  var last_section = associations.lastElementChild;
+  if (displayType) {
+    last_section.css("display", "block");
+    var clicked = "True"
+} else {
+    last_section.css("display", "none");
+  }
+}
+
+// Hide The Last Row
+function hideLastRow() {
+  // if ($("#associations").length > 0){
+  displayGrade(false);
+  }
+}
+
+$(hideLastRow);
+
 
 $('.smoothScroll').click(function() {
   if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
@@ -53,6 +88,7 @@ $('.smoothScroll').click(function() {
   }
 });
 });
+
 // Change the speed to whatever you want
 // Personally i think 1000 is too much
 // Try 800 or below, it seems not too much but it will make a difference
